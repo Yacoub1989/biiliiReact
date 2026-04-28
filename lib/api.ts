ed8e018b1c2accabb3d54b1fda8413cd61c47bfb
yacoub T'FEIL <yacoubsalemtfeil@yahoo.fr>
2026-04-28 09:45:24 +0000

    fix 21

--- lib/api.ts
+++ lib/api.ts
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "http://biiliiBackend.eba-phvqmhxm.eu-north-1.elasticbeanstalk.com/api",
+  //baseURL: "http://biiliiBackend.eba-phvqmhxm.eu-north-1.elasticbeanstalk.com/api",
+  baseURL: "/api-backend",
 });
 
 console.log("API URL =", process.env.NEXT_PUBLIC_API_URL);
--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async rewrites() {
+    return [
+      {
+        source: "/api-backend/:path*",
+        destination: "http://biiliiBackend.eba-phvqmhxm.eu-north-1.elasticbeanstalk.com/api/:path*",
+      },
+    ];
+  },
 };
 
 export default nextConfig;
