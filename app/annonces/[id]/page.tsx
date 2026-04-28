8d474453018ed91c57644928b96f30c6d94fa658
yacoub T'FEIL <yacoubsalemtfeil@yahoo.fr>
2026-04-28 07:52:57 +0000

    fix 6

--- app/annonces/[id]/page.tsx
+++ app/annonces/[id]/page.tsx
@@ -2,7 +2,7 @@
 
 import { useEffect, useState } from "react";
 import { getAdById } from "@/services/ads.service";
-import { Ad } from "@/types/Ad";
+import { Ad } from "@/app/types/Ad";
 import { formatPrice, getAdMainImage, toAbsoluteImageUrl } from "@/lib/ad-utils";
 
 type Props = {
